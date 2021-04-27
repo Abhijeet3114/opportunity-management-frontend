f95c7d696dbd2608ad81d29b6344d969c48570fe
Abhijeet3114 <chavanabhijeet3114@gmail.com>
2021-04-27 11:14:29 +0530

    Opportunity Services more tests added

--- src/app/opportunity.service.spec.ts
+++ src/app/opportunity.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { OpportunityService } from './opportunity.service';
 import { of } from 'rxjs';
 import { Opportunity } from './opportunity';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
 
 describe('OpportunityService', () => {
   let service: OpportunityService;
@@ -10,11 +11,12 @@ describe('OpportunityService', () => {
   let newOppo: Opportunity;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     service = TestBed.inject(OpportunityService);
     httpClient = TestBed.get(HttpClient);
-    newOppo = { "opportunityId": 10, "skills": "Angular, React, Java, Spring Framework, MySQL", "publishDate": new Date("2021-04-21"), "joiningDate": new Date("2021-04-21"), "description": "", "role": "Full Stack Developer", "joiningLocation": "Delhi", "experience": 4, "hiringManager": "Balpreet Singh" };
+    newOppo = { "opportunityId": 10, "skills": "Angular, React, Java, Spring Framework, MySQL", "publishDate": new Date("2021-04-21"), "joiningDate": new Date("2021-04-21"), "description": "", "role": "Full Stack Developer", "joiningLocation": "Delhi", "experience": 4, "hiringManager": "Abhijeet Chavan" };
   });
 
   it('should able to create Opporunity service', () => {
@@ -28,12 +30,85 @@ describe('OpportunityService', () => {
     expect(httpClient.get).toHaveBeenCalled();
   });
 
+  it("getOpportunities API call(with correct parameters) Should be able to return List of Opportunities", () => {
+    //create a mock return response
+    const Opportunities_response = [
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      },
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-27"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Abhijeet Chavan",
+      }
+    ];
+
+    let response;
+    spyOn(service, "getOpportunities").and.returnValue(of(Opportunities_response));
+    service.getOpportunities().subscribe(res => {
+      response = res;
+    });
+    console.log("Valid Opportunities",response);
+    expect(response).toBeTruthy(Opportunities_response);
+  });
+
   it('Should Able To Call getAllOpportunities API', () => {
     spyOn(httpClient, 'get').and.returnValue(of([]));
     service.getAllOpportunities();
     expect(service.getAllOpportunities()).toBeTruthy();
     expect(httpClient.get).toHaveBeenCalled();
   });
+
+  it("getAllOpportunities API call(with correct parameters) Should be able to return List of Opportunities", () => {
+    //create a mock return response
+    const Opportunities_response = [
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      },
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-27"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Abhijeet Chavan",
+      }
+    ];
+
+    let response;
+    spyOn(service, "getAllOpportunities").and.returnValue(of(Opportunities_response));
+    service.getAllOpportunities().subscribe(res => {
+      response = res;
+    });
+    console.log("All Opportunities", response);
+    expect(response).toBeTruthy(Opportunities_response);
+  });
+
 
   it('Should Able To Call createOpportunity API', () => {
     spyOn(httpClient, 'post').and.returnValue(of(Object));
@@ -42,11 +117,62 @@ describe('OpportunityService', () => {
     expect(httpClient.post).toHaveBeenCalled();
   });
 
+  it("createOpportunity API call Should be able to create oppo to list of opportunities with correct parameters", () => {
+    //create a mock return response
+    const Opportunities_response =
+      {
+        "opportunityId": 1,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      }
+    ;
+
+    let response;
+    spyOn(service, "createOpportunity").and.returnValue(of(Opportunities_response));
+    service.createOpportunity(newOppo).subscribe(res => {
+      response = res;
+    });
+    console.log("Created Opporunity", response);
+    expect(response).toBeTruthy(Opportunities_response);
+  });
+
+
+
   it('Should Able To Call getOpportunityByID API', () => {
     spyOn(httpClient, 'get').and.returnValue(of(Opportunity));
     service.getOpportunityById(10);
     expect(service.getOpportunityById(10)).toBeTruthy();
     expect(httpClient.get).toHaveBeenCalled();
+  });
+
+  it("getOpportunityByID API call(with correct parameters) Should be able to return Opportunity with specified ID", () => {
+    //create a mock return response
+    const Opportunities_response =
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      };
+
+    let response;
+    spyOn(service, "getOpportunityById").and.returnValue(of(Opportunities_response));
+    service.getOpportunityById(Opportunities_response.opportunityId).subscribe(res => {
+      response = res;
+    });
+    console.log("Valid Opportunities", response);
+    expect(response).toBeTruthy(Opportunities_response);
   });
 
   it('Should Able To Call deleteOpportunity API', () => {
@@ -56,11 +182,71 @@ describe('OpportunityService', () => {
     expect(httpClient.delete).toHaveBeenCalled();
   });
 
+  it("deleteOpportunity API call(with correct parameters) Should be able to delete Opportunity with specified ID", () => {
+    //create a mock return response
+    const Opportunities_response =
+    {
+      "opportunityId": 10,
+      "skills": "Angular, React, Java, Spring Framework, MySQL",
+      "publishDate": new Date("2021-04-21"),
+      "joiningDate": new Date("2021-04-21"),
+      "description": "",
+      "role": "Full Stack Developer",
+      "joiningLocation": "Delhi",
+      "experience": 4,
+      "hiringManager": "Balpreet Singh",
+    };
+
+    let response;
+    spyOn(service, "deleteOpportunity").and.returnValue(of(Opportunities_response));
+    service.deleteOpportunity(Opportunities_response.opportunityId).subscribe(res => {
+      response = res;
+    });
+    console.log("Deleted Opportunities", response);
+    expect(response).toBeTruthy(Opportunities_response);
+  });
+
   it('Should Able To Call searchOpportunities API', () => {
     spyOn(httpClient, 'get').and.returnValue(of([]));
     service.searchOpportunityByRole("Mumbai");
     expect(service.searchOpportunityByRole("Mumbai")).toBeTruthy();
     expect(httpClient.get).toHaveBeenCalled();
+  });
+
+  it("SearchOpportunity API call(with correct parameters) Should be able to return list of opportunities", () => {
+    //create a mock return response
+    const Opportunities_response =[
+    {
+      "opportunityId": 10,
+      "skills": "Angular, React, Java, Spring Framework, MySQL",
+      "publishDate": new Date("2021-04-21"),
+      "joiningDate": new Date("2021-04-21"),
+      "description": "",
+      "role": "Full Stack Developer",
+      "joiningLocation": "Delhi",
+      "experience": 4,
+      "hiringManager": "Balpreet Singh",
+      },
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Mumbai",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      }
+    ];
+
+    let response;
+    spyOn(service, "searchOpportunityByRole").and.returnValue(of(Opportunities_response));
+    service.searchOpportunityByRole("Mumbai").subscribe(res => {
+      response = res;
+    });
+    console.log("SearchResult", response);
+    expect(response).toBeTruthy(Opportunities_response);
   });
 
   it('Should Able To Call updateOpportunity API', () => {
@@ -70,4 +256,39 @@ describe('OpportunityService', () => {
     expect(httpClient.put).toHaveBeenCalled();
   });
 
+  it("deleteOpportunity API call Should be able to call delete oppo from list of opportunities with correct parameters", () => {
+    //create a mock return response
+    const Opportunities_response = [
+      {
+        "opportunityId": 1,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Delhi",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      },
+      {
+        "opportunityId": 10,
+        "skills": "Angular, React, Java, Spring Framework, MySQL",
+        "publishDate": new Date("2021-04-21"),
+        "joiningDate": new Date("2021-04-21"),
+        "description": "",
+        "role": "Full Stack Developer",
+        "joiningLocation": "Mumbai",
+        "experience": 4,
+        "hiringManager": "Balpreet Singh",
+      }
+    ];
+
+    let response;
+    spyOn(service, "updateOpportunity").and.returnValue(of(Opportunities_response));
+    service.updateOpportunity(10, newOppo).subscribe(res => {
+      response = res;
+    });
+    console.log("Update Opporunity", response);
+    expect(response).toBeTruthy(Opportunities_response);
+  });
 });
