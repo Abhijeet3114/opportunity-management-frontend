64e3e1fc5ff330ad01f075c0fc935ad45733e47b
Abhijeet3114 <chavanabhijeet3114@gmail.com>
2021-05-05 22:26:56 +0530

    Added bottom margin for footer

--- src/app/login-home/login-home.component.spec.ts
+++ src/app/login-home/login-home.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginHomeComponent } from './login-home.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { SocialAuthServiceConfig } from 'angularx-social-login'
+import { SocialLoginModule, GoogleLoginProvider } from 'angularx-social-login'
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 
 describe('LoginHomeComponent', () => {
   let component: LoginHomeComponent;
@@ -8,7 +13,26 @@ describe('LoginHomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginHomeComponent ]
+      imports: [HttpClientModule, AppRoutingModule, SocialLoginModule],
+      declarations: [LoginHomeComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+      ,
+      providers: [
+        {
+          provide: 'SocialAuthServiceConfig',
+          useValue: {
+            autoLogin: false,
+            providers: [
+              {
+                id: GoogleLoginProvider.PROVIDER_ID,
+                provider: new GoogleLoginProvider(
+                  '604014241218-os1ne12jcdmupgqospkes04ikjurq0fj.apps.googleusercontent.com'
+                )
+              }
+            ]
+          } as SocialAuthServiceConfig,
+        },
+      ],
     })
     .compileComponents();
   });
@@ -19,7 +43,7 @@ describe('LoginHomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create an instance of LoginHomeComponent', () => {
     expect(component).toBeTruthy();
   });
 });
